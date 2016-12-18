b5835404f15459d6e284a2800d8f57b6b5abd8d0
alain <alain@foo.com>
2016-12-18 18:24:05 +0100

    plotting

--- show-temp.py
+++ show-temp.py
@@ -2,16 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import paho.mqtt.client as mqtt
+from matplotlib import pyplot
+
+pyplot.ion () # mode interactif
+pData = [0] * 20
+fig = pyplot.figure ()
+pyplot.title (u'Affichage Température')
+axes = pyplot.axes ()
+l1, = pyplot.plot (pData)
+pyplot.ylim ([-10,40])
 
 # connexion serveur MQTT Mosquito
 def on_connect (client, userdata, flags, rc):
     print "connecté avec code: {code}".format (code=str(rc))
 
 def on_subscribe (client, userdata, mid, granted_qos):
-    print ("sub: " + granted_qos[0])
+    print "sub: {q}".format (q=str(granted_qos[0]))
 
 def on_message (client, userdata, msg):
-    print msg.topic
+    print "msg: {t}:{p}".format (t=msg.topic, p=str(msg.payload))
+    pData.append (float(msg.payload))
+    pyplot.ylim ([-10,40])
+    del pData [0]
+    l1.set_xdata([i for i in xrange(20)])
+    l1.set_ydata(pData) # update  the data
+    pyplot.draw ()
 
 def on_disconnect (client, userdata, rc):
     print "déconnecté: {code}".format (code=str(rc))
@@ -29,7 +44,7 @@ client.on_disconnect = on_disconnect
 client.on_log = on_log
 
 try:
-    client.connect (host='127.0.0.1', port=1883, bind_address='127.0.0.1')
+    client.connect (host='127.0.0.1', port=1883)
 except Exception as e:
     print e
     exit (1)
